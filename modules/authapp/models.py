0648620ea22aa02d2cc0798cdfbf0fe5052e77ce
jhabinder.webkrone <jhabinder.webkrone@gmail.com>
2021-10-23 13:20:22 +0530

    .

--- modules/authapp/models.py
+++ modules/authapp/models.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
@@ -35,4 +34,3 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.first_name
->>>>>>> b7518fbe365aee572bca4196084771f764a6b268
