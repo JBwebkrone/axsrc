39ef059cc8b3b469e5125f54bb9f23e22656b29e
jhabinder.webkrone <jhabinder.webkrone@gmail.com>
2021-10-23 13:18:59 +0530

    Added login

--- modules/authapp/urls.py
+++ modules/authapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import (
     SignupView,
+    LoginView,
 )
 
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(), name="login"),
     path('signup/', SignupView.as_view(), name="signup"),
 ]
--- modules/authapp/views.py
+++ modules/authapp/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.conf import settings
 from django.urls.base import reverse
+from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView
 from .forms import (
     CustomUserCreationForm,
@@ -12,4 +13,8 @@ class SignupView(CreateView):
     template_name = "registration/signup.html"
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
+
+
+class LoginView(LoginView):
+    template_name = "registration/login.html"
     
